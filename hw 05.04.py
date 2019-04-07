c39a4394b48ba80b8b38880d3bf46247ac23bb03
Dmitry <evmenov.d.a@gmail.com>
2019-04-07 12:14:56 +0300

    hw

--- hw 05.04.py
+++ hw 05.04.py
@@ -4,19 +4,30 @@ num = []
 for i in range(3):
     num.append(random.randint(1, 4))
 print(num)
+n = 0
 dub = 0
+sum4 = 0
 num.sort()
 for i in range(len(num)-1):
     if num[i] == num[i+1]:
         dub += 1
-    if num[i] + num[i+1] == 4:
-        print('yes! 4')
-    if i == range(len(num)):
-        break
 if dub != 0:
-    print('yes')
+    print('doubles: yes:', dub)
 else:
-    print('error')
+    print('doubles: no')
+print('---')
+print(num)
+if num[0] + num[1] == 4:
+        sum4 += 1
+if num[0] + num[2] == 4:
+        sum4 += 1
+if num[1] + num[2] == 4:
+        sum4 += 1
+if sum4 != 0:
+    print('sum = 4: yes:', sum4)
+else:
+    print('sum = 4: no')
+print('---')
 
 sum_num2 = 0
 num2 = [i for i in range(14+1)]
@@ -27,3 +38,20 @@ for i in range(len(num2)):
 sum_num3 = sum(num2)
 print(sum_num2)
 print(sum_num3)
+print('---')
+
+days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+for i in range(len(days)):
+    if i < 6+1:
+        print(i+1, days[i], '- weekday')
+    else:
+        print(i+1, days[i], '- weekend')
+print('---')
+
+for i, n in enumerate(days, 1):
+    if i < 6:
+        print(i, n, '- weekday')
+    else:
+        print(i, n, '- weekend')
+print('---')
