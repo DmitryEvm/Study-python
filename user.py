5e55315d788bbb4162c85de824729cca5a63d4ab
EvmenovDmitry <evmenov.d.a@gmail.com>
2019-04-23 14:26:09 +0300

    Delete user.py

--- user.py
+++
@@ -1,7 +0,0 @@
-user_name = input("What is your name? ")
-user_age = input("How old are you? ")
-user_live = input("Where are you live? ")
-print(f''''
-This is {user_name}
-It is {user_age}
-(S)he live in {user_live}''')
